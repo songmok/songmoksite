7d171a97095bf6035fa758ba310407ef857779f2
songmok <songmokseed@gmail.com>
2023-10-23 17:11:35 +0900

    배포 테스트

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
-export const DATABASE_ID = process.env.NOTION_DATABASE_ID;
-export const DATABASE_ID2 = process.env.NOTION_DATABASE_ID2;
-export const TOKEN = process.env.NOTION_TOKEN;
-export const TISTORY_APP = process.env.TISTORY_APP_ID;
-export const TISTORY_SCT = process.env.TISTORY_SECRET_KEY;
-export const TISTORY_ACCES = process.env.TISTORY_TOKEN;
+export const DATABASE_ID = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID;
+export const DATABASE_ID2 = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID2;
+export const TOKEN = process.env.NEXT_PUBLIC_NOTION_TOKEN;
+export const TISTORY_APP = process.env.NEXT_PUBLIC_TISTORY_APP_ID;
+export const TISTORY_SCT = process.env.NEXT_PUBLIC_TISTORY_SECRET_KEY;
+export const TISTORY_ACCES = process.env.NEXT_PUBLIC_TISTORY_TOKEN;
