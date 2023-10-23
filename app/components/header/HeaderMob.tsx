dbba186896fc37d8b3b6ab267e33f87ebc780833
songmok <songmokseed@gmail.com>
2023-10-23 17:58:26 +0900

    -- 개발 중 화면인 tistory 주석처리

--- app/components/header/HeaderMob.tsx
+++ app/components/header/HeaderMob.tsx
@@ -24,9 +24,9 @@ const HeaderMob = () => {
           <Link className=" hover:text-gray-900" href="/portfolio">
             Portfolio
           </Link>
-          <Link className=" mr-5 hover:text-gray-900" href="/tistory">
+          {/* <Link className=" mr-5 hover:text-gray-900" href="/tistory">
             Tistory
-          </Link>
+          </Link> */}
           <Link className=" hover:text-gray-900" href="/life">
             Life
           </Link>
