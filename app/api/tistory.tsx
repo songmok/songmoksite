d67ebefb10566253c551134d70fa68edcb14cd21
songmok <songmokseed@gmail.com>
2023-10-23 16:57:33 +0900

    -- 배포 오류 수정

--- app/api/tistory.tsx
+++ app/api/tistory.tsx
@@ -1,10 +1,10 @@
 import { TISTORY_ACCES } from "../../config";
-import { Ttoken } from "../../token";
+// import { Ttoken } from "../../token";
 
 export const JsonType = "&output=json";
 export const BlogName = "&blogName=songmok-blog";
 export const PageNumber = "&page=1";
-export const TtokenAndJson = `?access_token=${Ttoken}${JsonType}`;
+export const TtokenAndJson = `?access_token=${TISTORY_ACCES}${JsonType}`;
 export const BlogInfo = "https://www.tistory.com/apis/blog/info";
 export const BlogList = "https://www.tistory.com/apis/post/list";
 export const BlogCate = "https://www.tistory.com/apis/category/list";
